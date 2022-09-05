fa96c2f8ed8bdf13987b9648d1d40844dd7c85b5
Vlad Karmazin <karmazin00@gmail.com>
2022-09-06 02:03:28 +0500

    Added task description

---
+++ src/tasks/CountLetters.kt
@@ -0,0 +1,9 @@
+package tasks
+
+/**
+ * @author Vladislav Karmazin
+ */
+fun main() {
+    // AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBB -> A4B3C2XYZD4E3F3A6B28
+    // O(n^2) хотя бы
+}
---
+++ src/tasks/GroupWordsByCommonLetters.kt
@@ -0,0 +1,13 @@
+package tasks
+
+/**
+ * @author Vladislav Karmazin
+ */
+fun main() {
+    /**
+     * Sample Input ["eat", "tea", "tan", "ate", "nat", "bat"]
+     * Sample Output [ ["ate", "eat", "tea"], ["nat", "tan"], ["bat"] ]
+     * Т.е. сгруппировать слова по "общим буквам"
+     * // groupBy
+     */
+}
